c0fd592a9c49b53436a49ac792c140dc07365cbe
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-09 22:43:46 +0300

    Concert edit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const Concert = require('./models/concerts')
+const methodOverride = require('method-override')
 
 main().catch((err) => console.log(err))
 
@@ -16,6 +17,7 @@ async function main() {
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -36,8 +38,18 @@ app.post('/concerts', async (req, res) => {
 
 app.get('/concerts/:id', async (req, res) => {
   const { id } = req.params
-  const conc = await Concert.findById(id)
-  res.render('concerts/show', { conc })
+  const concert = await Concert.findById(id)
+  res.render('concerts/show', { concert })
+})
+
+app.get('/concerts/:id/edit', async (req, res) => {
+  const concert = await Concert.findById(req.params.id)
+  res.render('concerts/edit', { concert })
+})
+app.put('/concerts/:id', async (req, res) => {
+  const { id } = req.params
+  const concert = await Concert.findByIdAndUpdate(id, { ...req.body.concert })
+  res.redirect(`/concerts/${concert._id}`)
 })
 
 app.listen(3000, () => {
