3a5758ca8126c09dd830f5a0cbe103919a2c6c11
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-11 10:58:54 +0300

    Error template

--- app.js
+++ app.js
@@ -86,8 +86,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something Went Wrong' } = err
-  res.status(statusCode).send(message)
+  const { statusCode = 500 } = err
+  if (!err.message) err.message = 'Somthing Went Wrong'
+  res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
