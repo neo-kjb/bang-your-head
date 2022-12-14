1665274a9b248f4f7d0e398ff1b6e502d93a9a6c
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-14 21:53:38 +0300

    Concert delete middleware

--- models/concerts.js
+++ models/concerts.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Review = require('./review')
 
 const ConcertSchema = new Schema({
   title: String,
@@ -13,6 +14,16 @@ const ConcertSchema = new Schema({
       ref: 'Review',
     },
   ],
+})
+
+ConcertSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    })
+  }
 })
 
 module.exports = mongoose.model('concert', ConcertSchema)
