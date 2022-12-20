70ee57aee0dfcf7ff6c8888d9c339d20dda21741
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-20 13:31:18 +0300

    Login route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 const User = require('../models/user')
 
@@ -22,6 +23,22 @@ router.post(
       res.redirect('/register')
     }
   }),
+)
+
+router.get('/login', (req, res) => {
+  res.render('users/login')
+})
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login',
+  }),
+  (req, res) => {
+    req.flash('success', 'Welcome back!')
+    res.redirect('/concerts')
+  },
 )
 
 module.exports = router
