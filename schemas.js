a4b4a03c63d2b4dba806462aeb54e403f8a36524
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-25 20:41:47 +0300

    'Delete' images form

--- schemas.js
+++ schemas.js
@@ -1,20 +1,20 @@
-const { required } = require('joi')
 const joi = require('joi')
 module.exports.concertSchema = joi.object({
   concert: joi
     .object({
       title: joi.string().required(),
       price: joi.number().required().min(0),
-      images: joi.array().items(
-        joi.object({
-          url: joi.string().required(),
-          filename: joi.string().required(),
-        }),
-      ),
+      // images: joi.array().items(
+      //   joi.object({
+      //     url: joi.string().required(),
+      //     filename: joi.string().required(),
+      //   }),
+      // ),
       location: joi.string().required(),
       description: joi.string().required(),
     })
     .required(),
+  deleteImages: joi.array(),
 })
 
 module.exports.reviewSchema = joi.object({
