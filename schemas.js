ca57110f4d2d0d5d799dcfada327cb09a7dfc4cc
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-28 04:10:40 +0300

    Sanitizing HTML

--- schemas.js
+++ schemas.js
@@ -1,8 +1,33 @@
-const joi = require('joi')
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+    'string.escapeHTML': '{{#label}} must not include HTML!',
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        })
+        if (clean !== value)
+          return helpers.error('string.escapeHTML', { value })
+        return clean
+      },
+    },
+  },
+})
+
+const joi = BaseJoi.extend(extension)
+
 module.exports.concertSchema = joi.object({
   concert: joi
     .object({
-      title: joi.string().required(),
+      title: joi.string().required().escapeHTML(),
       price: joi.number().required().min(0),
       // images: joi.array().items(
       //   joi.object({
@@ -10,8 +35,8 @@ module.exports.concertSchema = joi.object({
       //     filename: joi.string().required(),
       //   }),
       // ),
-      location: joi.string().required(),
-      description: joi.string().required(),
+      location: joi.string().required().escapeHTML(),
+      description: joi.string().required().escapeHTML(),
     })
     .required(),
   deleteImages: joi.array(),
@@ -21,7 +46,7 @@ module.exports.reviewSchema = joi.object({
   review: joi
     .object({
       rating: joi.number().required().min(1).max(5),
-      body: joi.string().required(),
+      body: joi.string().required().escapeHTML(),
     })
     .required(),
 })
