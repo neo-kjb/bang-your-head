d806a5b998703a46e3f79033e12bad1ad26da810
neo-kjb <anaszawahreh.dev@gmail.com>
2022-12-28 03:35:14 +0300

    Mongo injection

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const ExpressError = require('./utils/ExpressError')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
 
 const userRoutes = require('./routes/users')
 const concertRoutes = require('./routes/concerts')
@@ -36,6 +37,11 @@ app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  }),
+)
 
 const sessionConfig = {
   secret: '†††|ŽÖŽ4))¹)3Ö)',
